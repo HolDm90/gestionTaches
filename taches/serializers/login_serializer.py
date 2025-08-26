575249917657b4602e29f415c3530bf68141e618
Holdm <denahouholvanus@gmail.com>
2025-08-26 01:17:11 +0100

    mise ajout de taches/serializers/login_serializer.py

--- taches/serializers/login_serializer.py
+++ taches/serializers/login_serializer.py
@@ -3,21 +3,39 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from taches.models.user_model import User
 
+
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    username_field = User.USERNAME_FIELD  # généralement 'username'
+    @classmethod
+    def get_token(cls, user):
+        """
+        Personnalisation du contenu du JWT.
+        Tout ce qui est mis ici sera encodé dans le token (access/refresh).
+        """
+        token = super().get_token(user)
+        token['role'] = user.role
+        token['is_validated'] = user.is_validated
+        token['email'] = user.email
+        token['username'] = user.username
+        return token
 
     def validate(self, attrs):
+        """
+        Personnalisation de la réponse du login.
+        """
         data = super().validate(attrs)
 
-        # Vérification que l'utilisateur est validé
+        # Vérifie si le compte est validé par l'admin
         if not self.user.is_validated:
-            raise serializers.ValidationError("Votre compte n'a pas encore été validé par l'administrateur.")
+            raise serializers.ValidationError(
+                "Votre compte n’a pas encore été validé par un administrateur."
+            )
 
-        # Infos supplémentaires à renvoyer
+        # Ajouter des infos dans la réponse JSON du login
         data.update({
-            'id': self.user.id,
-            'username': self.user.username,
-            'email': self.user.email,
-            'role': self.user.role
+            "id": self.user.id,
+            "username": self.user.username,
+            "email": self.user.email,
+            "role": self.user.role,
+            "is_validated": self.user.is_validated,
         })
         return data
